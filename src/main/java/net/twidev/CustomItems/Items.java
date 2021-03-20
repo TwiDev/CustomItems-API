e27fff3b9cfa70adf26bda81f975339d0cba0b56
TwiDev <twidev5@gmail.com>
2021-03-20 09:56:14 +0100

    add items javaPlugin

---
+++ src/main/java/net/twidev/CustomItems/Items.java
@@ -0,0 +1,40 @@
+package net.twidev.CustomItems;
+
+import net.twidev.CustomItems.listeners.InventoryEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author TwiDev
+ */
+public class Items extends JavaPlugin {
+
+    private static Items instance;
+
+    public void log(String message) {
+        this.getLogger().info(message);
+    }
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        log("#==========[WELCOME TO CUSTOM ITEMS API]===========#");
+        log("# CustomItemsAPI by TwiDev is now loading ...      #");
+        log("# Thank for using custom items api !               #");
+        log("#==================================================#");
+
+        getServer().getPluginManager().registerEvents(new InventoryEvent(), this);
+
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        this.setEnabled(false);
+    }
+
+    public static Items getInstance() {
+        return instance;
+    }
+}
